26a7fe0d92de7eed6961e690f51dba8f222abbf3
Brian Wong <bwong92@gmail.com>
2019-07-30 07:23:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each.with_index do |dwarf,idx|
+    puts "#{idx + 1}. #{dwarves[idx]}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.map do |w|
+    w[0].upcase + w[1..-1].downcase + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |food|
+    return food if cheese_types.include?(food)
+  end
+  nil
 end
